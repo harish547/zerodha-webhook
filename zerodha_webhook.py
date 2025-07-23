b8cc6b6abde82910bf5201f7589aa274c03ee26c
harish547 <pathakh82@gmail.com>
2025-07-23 20:24:49 +0530

    Update zerodha_webhook.py

--- zerodha_webhook.py
+++ zerodha_webhook.py
@@ -49,5 +49,8 @@ def home():
     return "✅ Zerodha Webhook Server is Running!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
+    import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
+
 
